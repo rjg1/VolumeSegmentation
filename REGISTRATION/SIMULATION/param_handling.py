31c5d7e7086d32f41364a9929eb205d99aeae8fb
rjg1 <rj.gibbons12@gmail.com>
2025-05-06 18:53:50 +1000

    started commenting parameter descriptions

--- REGISTRATION/SIMULATION/param_handling.py
+++ REGISTRATION/SIMULATION/param_handling.py
@@ -4,18 +4,18 @@ import numpy as np
 PLANE_GEN_PARAMS_DEFAULT = {
     "read_filename" : None, # file to read plane parameters from
     "save_filename" : None, # file to save plane parameters to 
-    "anchor_intensity_threshold": 0.8,
-    "align_intensity_threshold": 0.4,
-    "z_threshold": 5,
-    "max_tilt_deg": 30.0,
-    "projection_dist_thresh":  0.5,
+    "anchor_intensity_threshold": 0.8, # threshold to compare transformed intensity to for a point to be considered an anchor pt (see transform_intensity)
+    "align_intensity_threshold": 0.4, # as above, but for alignment points
+    "z_threshold": 5, # max distance in z between two points in a plane
+    "max_tilt_deg": 30.0, # max magnitude of tilt in the vector between two points in a plane
+    "projection_dist_thresh":  0.5, # euclidean distance for a point considered close enough to a plane for it to be projected
     "transform_intensity" : "quantile", # "quantile", "raw", "minmax" - transforms avg intensity per roi and compares to anchor/align intensity threshold
     "plane_boundaries" : [-np.inf, np.inf, -np.inf, np.inf],
     "margin" : 2, # distance between boundary point and pixel in img to be considered an edge roi
-    "match_anchors" : True,
-    "fixed_basis" : True,
+    "match_anchors" : True, # require two planes to have a common anchor to be equivalent (turning this off will reduce the number of planes and accuracy)
+    "fixed_basis" : True, # define the projected 2d x-axis as [1,0,0]
     "regenerate_planes" : False, # always re-generate planes even if this z-stack has a set
-    "max_alignments" : 500,
+    "max_alignments" : 500, # maximum number of alignment points allowed per plane
     "z_guess": -1, # guess at the z-level where the plane match is located in stack-> -1 means no guess
     "z_range": 0 # +- tolerance to search for in z in both planes
 }
